e32912e5f5d7de1b6d5dc318654c4603d427186b
Lauraine25 <laurainekiro14@gmail.com>
2024-06-06 08:17:06 -0700

    add login.php file

---
+++ login.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title><link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <header class="header">
+        <nav class="lienentete">
+            <a href="#">Home</a>
+            <a href="#">A propos</a>
+            <a href="#">Services</a>
+            <a href="#">Contacts</a>
+        </nav>
+      
+        <form action="#" class="search-bar">
+            <input type="text"placeholder="Rechercher...">
+<button type="submit"><img src="images/search.png" width=24 alt=""></button>
+        </form>
+        
+   
+
+    </header>
+    <div class="container">
+      <div class="content">
+          <h2 class="bx bxl-firebase">Boulangerie Maseka </h2>
+
+          <div class="div text-si">
+              <h2>Bienvenue <br> <span>A la Boulangerie Maseka</span></h2>
+              <p> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Suscipit fugiat, similique autem quidem saepe consequuntur ratione rerum, ipsam exercitationem magnam accusantium illo ad at, impedit laboriosam architecto voluptate ex unde.</p>
+              <div class="div social-icons">
+                  <a href="#"><img src="" alt=""></a>
+                  <a href="#"><img src="" alt=""></a>
+                  <a href="#"><img src="" alt=""></a>
+                  <a href="#"><img src="" alt=""></a>
+              </div>
+          </div>
+          <div class="div logreg-box">
+
+          </div>
+      </div>
+      </div>
+    
+</body>
+</html>
